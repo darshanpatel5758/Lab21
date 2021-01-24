92a91211997f06b1de4cb6e3336e2e2d266a6391
darshanpatel5758 <darshanpatel5758@gmail.com>
2021-01-24 17:05:36 -0500

    lab 21

---
+++ lab 21/Controllers/MovieController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using lab_21.Models.Movie;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab_21.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+
+        public IActionResult MovieResult(MovieViewModel model)
+        {
+
+            return View(model.Title);
+        }
+
+    }
+}
